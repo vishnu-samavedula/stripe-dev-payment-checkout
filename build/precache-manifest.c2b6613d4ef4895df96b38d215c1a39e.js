fa260c490668ab4b261d97a93b0c249a226376e5
Vishnu Samavedula <vishnusamavedula@vishnus-mbp.lan>
2023-08-16 21:51:29 -0700

    stripe checkout example

---
+++ build/precache-manifest.c2b6613d4ef4895df96b38d215c1a39e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482c24977232673d24f3ca8672444433",
+    "url": "/checkout/index.html"
+  },
+  {
+    "revision": "70c412f927d566eb14e0",
+    "url": "/checkout/static/css/main.bda59e25.chunk.css"
+  },
+  {
+    "revision": "d674e06263ade036bd45",
+    "url": "/checkout/static/js/2.285bf17a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/checkout/static/js/2.285bf17a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c412f927d566eb14e0",
+    "url": "/checkout/static/js/main.b4e48b46.chunk.js"
+  },
+  {
+    "revision": "1d4ba4c9967c8cb86da4",
+    "url": "/checkout/static/js/runtime-main.6883fe7f.js"
+  }
+]);
